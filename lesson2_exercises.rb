7f636a6fa493728c193673b41b27588928e5961d
Andrew Pacheco <ndrewpacheco@gmail.com>
2019-04-02 12:28:00 -0700

    first commit

---
+++ lesson2_exercises.rb
@@ -0,0 +1,13 @@
+class Dog
+  def speak
+    'bark!'
+  end
+
+  def swim
+    'swimming!'
+  end
+end
+
+teddy = Dog.new
+puts teddy.speak           # => "bark!"
+puts teddy.swim           # => "swimming!"
