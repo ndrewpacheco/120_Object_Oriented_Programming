3700cbd94dedd46bfd9cdf8f5d0cea959eea4bd0
Andrew Pacheco <ndrewpacheco@gmail.com>
2019-06-05 01:55:54 -0500

    fin oobasics accessor methods

---
+++ lesson_5/exercises/oo_basics_accessor_methods/ex10.rb
@@ -0,0 +1,29 @@
+
+# Give us your feedback
+# Unexpected Change
+# Modify the following code to accept a string containing a first and last name. 
+# The name should be split into two instance variables in the setter method, 
+# then joined in the getter method to form a full name.
+# # 
+
+class Person
+  
+
+  def name=(name)
+    split_name = name.split
+    @first_name = split_name.first
+    @last_name = split_name.last
+  end
+
+  def name
+    #{}"#{@first_name} #{@last_name}"
+    @first_name + " " + @last_name
+  end
+end
+
+person1 = Person.new
+person1.name = 'John Doe'
+puts person1.name
+# Expected output:
+
+# John Doe
