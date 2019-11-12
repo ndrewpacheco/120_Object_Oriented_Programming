5c5fabe428499b40bde31c7ad5803ba6e4812866
Andrew Pacheco <ndrewpacheco@gmail.com>
2019-11-12 18:26:26 -0500

    working on inehritance

---
+++ round2/OOBasics_Classes_Objs/counting_cats.rb
@@ -0,0 +1,24 @@
+# Counting Cats
+# Using the following code, create a class named Cat that tracks 
+#the number of times a new Cat object is instantiated. 
+
+# The total number of Cat instances should be printed when ::total is invoked.
+
+class Cat
+  @@total_cats = 0
+
+  def initialize
+    @@total_cats += 1
+  end
+  def self.total
+    p @@total_cats
+  end
+
+end
+kitty1 = Cat.new
+kitty2 = Cat.new
+
+Cat.total
+# Expected output:
+
+# 2
