b89d37c197fd301b272d388e8f6ea534e3076c74
Andrew Pacheco <ndrewpacheco@gmail.com>
2019-11-15 12:48:55 -0500

    working on exercises

---
+++ round2/OOBasics_accessor_methods/reading_writing.rb
@@ -0,0 +1,14 @@
+# Reading and Writing
+# Add the appropriate accessor methods to the following code.
+
+class Person
+end
+
+person1 = Person.new
+person1.name = 'Jessica'
+puts person1.name
+
+
+# Expected output:
+
+# Jessica
